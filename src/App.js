537145bae746e7e96c36153a5039f240b0c912b0
cic0184 <cic0184@gmail.com>
2024-02-14 01:17:32 +0900

    features: add-router-mode-toggle-css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import Profile from './pages/Client/Profile';
+import Registration from './pages/User/Profile/Registration';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/client/profile" element={<Profile />} />
+        <Route path="/client/project/registration" element={<Registration />} />
+        <Route path="/user/profile/registration" element={<Registration />} />
       </Routes>
     </Router>
   );
