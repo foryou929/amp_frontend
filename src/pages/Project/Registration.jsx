2a71197efd553b2a53855c9615a56a686289acf6
Alex Rio <ar02220604@gmail.com>
2024-02-22 14:18:44 +0900

    features: add-project-registration

--- src/pages/Project/Registration.jsx
+++ src/pages/Project/Registration.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import Button from "../../components/Button";
 import Image from "../../components/Image";
 import Input from "../../components/Input";
@@ -5,73 +7,105 @@ import RadioGroup from "../../components/RadioGroup";
 import Select from "../../components/Select";
 import Textarea from "../../components/Textarea";
 
+import query from "../../utils/query";
+import { NotificationManager } from "react-notifications";
+
 const Registration = () => {
+    const [project, setProject] = useState({
+        name: "",
+        points: 0,
+        recruitment_content: 0,
+        space_type: 0,
+        recruitment_period: 0,
+        area: 0,
+        year_designation: 0,
+        recruitment_number: 0,
+        project_period: 0,
+        content_size: "",
+        description: "",
+    });
+
+    const onChange = (target) => {
+        const newProject = { ...project };
+        newProject[target?.name] = target?.value;
+        setProject(newProject);
+        console.log(newProject)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        query.auth.post('/api/project', project, res => {
+            NotificationManager.success('Success');
+        });
+    }
+
     return (
         <>
             <h1 className="text-2xl font-bold">プロジェクトを作成</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <section className="my-4">
                     <h6>タイトル</h6>
-                    <Textarea />
+                    <Textarea name="name" onChange={(e) => onChange(e.target)} />
                 </section>
                 <section className="my-4">
                     <h6>金額</h6>
                     <div>
-                        <Input /> pt
+                        <Input name="points" onChange={(e) => onChange(e.target)} /> pt
                     </div>
                 </section>
                 <section className="my-4">
                     <h6>募集コンテンツ</h6>
                     <Select options={[
                         { value: 0, label: "ステッカー" }
-                    ]} />
+                    ]} name="recruitment_content" onChange={(target) => onChange(target)} />
                 </section>
                 <section className="my-4">
                     <h6>スペース種別</h6>
                     <Select options={[
                         { value: 0, label: "車の窓" }
-                    ]} />
+                    ]} name="space_type" onChange={(target) => onChange(target)} />
                 </section>
                 <section className="my-4">
                     <h6>募集期間</h6>
                     <Select options={[
                         { value: 0, label: "1週間" }
-                    ]} />
+                    ]} name="recruitment_period" onChange={(target) => onChange(target)} />
                 </section>
                 <section className="my-4">
                     <h6>エリア</h6>
                     <Select options={[
                         { value: 0, label: "東京都(23区内)" }
-                    ]} />
+                    ]} name="area" onChange={(target) => onChange(target)} />
                 </section>
                 <section className="my-4">
                     <h6>年代指定</h6>
                     <RadioGroup
-                        name="year"
+                        name="year_designation"
                         defaultValue={0}
                         options={[
                             { value: 0, label: "なし" },
-                            { value: 1, label: "あり"}
+                            { value: 1, label: "あり" }
                         ]}
+                        onChange={(target) => onChange(target)}
                     />
                 </section>
                 <section className="my-4">
                     <h6>募集数</h6>
-                    <Input /> 人
+                    <Input name="recruitment_number" onChange={(e) => onChange(e.target)} /> 人
                 </section>
                 <section className="my-4">
                     <h6>プロジェクト期間</h6>
                     <Select options={[
                         { value: 0, label: "3週間" }
-                    ]} />
+                    ]} name="area" onChange={(target) => onChange(target)} />
                 </section>
                 <section className="my-4">
                     <h6>コンテンツのサイズ等</h6>
-                    <Input />
+                    <Input name="content_size" onChange={(e) => onChange(e.target)} />
                 </section>
                 <section className="my-4">
                     <h6>説明文</h6>
-                    <Textarea />
+                    <Textarea name="description" onChange={(e) => onChange(e.target)} />
                 </section>
                 <section className="my-4">
                     <h6>コンテンツ見本画像</h6>
