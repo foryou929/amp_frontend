7d1f1fe165ea9bd5b8e2c7a3f0c67b7e8f274947
Alex Rio <ar02220604@gmail.com>
2024-02-22 15:06:45 +0900

    features: add-project-manage

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,7 +2,7 @@ const List = ({ items, className, ...props }) => {
     return (
         <ul className={`border-t border-gray-200 ${className}`} {...props}>
             {
-                items.map((item) => (
+                items?.map((item) => (
                     <li key={item.key} className="border-b border-gray-200 p-4">
                         {
                             item.content
--- src/pages/Project/Manage.jsx
+++ src/pages/Project/Manage.jsx
@@ -1,6 +1,57 @@
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+
+import Link from "../../components/Link";
+import List from "../../components/List";
+import ProjectItem from "../../components/ProjectItem";
+
+import query from "../../utils/query";
+
 const Manage = () => {
+    const [queryParameters] = useSearchParams();
+
+    const [active, setActive] = useState(1);
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        const page = queryParameters.get("page");
+        if (page)
+            setPage(page);
+    }, [queryParameters]);
+
+    useEffect(() => {
+        query.auth.get("/api/project", (res) => {
+            alert(1);
+        });
+    }, [page]);
+
     return (
         <>
+            <h1 className="text-2xl font-bold">プロジェクト</h1>
+            <div className="flex justify-between mt-4">
+                <p>20件</p>
+                <Link to={"/"} >終了したプロジェクト&gt;</Link>
+            </div>
+            <div className="flex mt-4">
+                <div className={`w-1/2 py-2 text-center ${active == 1 ? "bg-[#00146E] text-white" : "bg-[#F8F9FA] border border-gray-200"}`} onClick={() => setActive(1)}>
+                    <p>進行中</p>
+                    <p className="text-gray-400">12件</p>
+                </div>
+                <div className={`w-1/2 py-2 text-center ${active == 2 ? "bg-[#00146E] text-white" : "bg-[#F8F9FA] border border-gray-200"}`} onClick={() => setActive(2)}>
+                    <p>募集中</p>
+                    <p className="text-gray-400">8件</p>
+                </div>
+            </div>
+            <List items={[
+                {
+                    key: 0,
+                    content: <ProjectItem img={"/1"} status={"広告物発送"} title={"プロジェクトのタイトル"} point={"200Pt/I日〜"} type={"車広告"} detail={"提案数:4 募集数:1 期間:あと10日"} />
+                },
+                {
+                    key: 0,
+                    content: <ProjectItem img={"/1"} status={"広告物発送"} title={"プロジェクトのタイトル"} point={"200Pt/I日〜"} type={"車広告"} detail={"提案数:4 募集数:1 期間:あと10日"} />
+                }
+            ]} />
         </>
     )
 }
