257a2f078547f8ea088ce1493fd4cc7694be3709
Alex Rio <ar02220604@gmail.com>
2024-02-25 23:36:21 +0900

    features: add-message-Slice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit'
 import userSlice from "../common/userSlice";
+import messageSlice from "../common/messageSlice";
 
 const combinedReducer = {
-    user: userSlice
+    user: userSlice,
+    message: messageSlice
 }
 
 export default configureStore({
