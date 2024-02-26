a5467c402bbf66a9b0bb4eacbb721e3a39efcd13
Alex Rio <ar02220604@gmail.com>
2024-02-26 09:33:37 +0900

    features: complete-application-step

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,3 +1,8 @@
-export const RECRUITING = "recruiting";
+export const RECRUITING = "RECRUITING";
 export const PROGRESSING = "progressing";
 export const FINISH = "finish";
+
+export const SECTION = {
+    NORMAL: 0,
+    APPLY: 1,
+}
