9112eb4f50389fd6f692cf7a14082524df0b65cf
Alex Rio <ar02220604@gmail.com>
2024-02-28 11:32:59 +0900

    add-project-detail-page

--- src/pages/User/Layout.jsx
+++ src/pages/User/Layout.jsx
@@ -1,14 +1,14 @@
-import { Routes, Route } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
-import { user_menus } from "../menu";
 import Header from '../Header';
+import { user_menus } from "../menu";
 
 import Profile from '../User/Profile';
 import ProfileRegistration from './Profile/Registration';
 
+import ProjectDetail from '../Project/Detail';
 import ProjectManage from '../Project/Manage';
 import ProjectView from '../Project/View';
-import ProjectInfo from '../Project/Info';
 
 import SpaceManage from '../Space/Manage';
 import SpaceRegistration from '../Space/Registration';
@@ -29,7 +29,7 @@ const UserLayout = () => {
 
                     <Route path="/project/manage" element={<ProjectManage mode={"user"} />} />
                     <Route path="/project/view" element={<ProjectView mode={"user"} />} />
-                    <Route path="/project/info" element={<ProjectInfo mode={"user"} />} />
+                    <Route path="/project/detail" element={<ProjectDetail mode={"user"} />} />
 
                     <Route path="/space/manage" element={<SpaceManage />} />
                     <Route path="/space/registration" element={<SpaceRegistration />} />
