cfaff72ac280e8d0e328891645fbfe88335d5ad6
Alex Rio <ar02220604@gmail.com>
2024-02-29 17:22:10 +0900

    features: fix-none-data-string

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,13 +2,15 @@ const List = ({ items, className, ...props }) => {
     return (
         <ul className={`border-t border-gray-200 ${className}`} {...props}>
             {
-                items?.map((item) => (
-                    <li key={item.key} className="border-b border-gray-200 p-4">
-                        {
-                            item.content
-                        }
-                    </li>
-                ))
+                items.length == 0 ?
+                    <p className="py-4 text-center border-b border-gray-200">データはありません。</p>
+                    : items?.map((item) => (
+                        <li key={item.key} className="border-b border-gray-200 p-4">
+                            {
+                                item.content
+                            }
+                        </li>
+                    ))
             }
         </ul>
     )
