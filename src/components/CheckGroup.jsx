da409b3f42c8d4e8c2838d4b878c91996ff47b3c
Alex Rio <ar02220604@gmail.com>
2024-02-29 17:21:40 +0900

    features: fix-checkgroup

--- src/components/CheckGroup.jsx
+++ src/components/CheckGroup.jsx
@@ -1,27 +1,44 @@
 import { useEffect, useState } from "react";
 import Checkbox from "./Checkbox";
 
-const CheckGroup = ({ className, itemClassName, name, label, items, defaultValue, onChange }) => {
-    const [value, setValue] = useState(defaultValue || 0)
-    const onCheckChange = (index, e) => {
-        if (e.target.checked) {
-            setValue(value | (1 << index));
-        } else {
-            setValue(value & (~(1 << index)));
-        }
-    }
+const CheckGroup = ({ name, label, className, items, itemClassName, onChange, value }) => {
+    const [status, setStatus] = useState(0)
+
     useEffect(() => {
-        if (onChange)
-            onChange({ name, value });
+        setStatus(value);
     }, [value]);
+
     return (
         <div className={className}>
             <label className="py-0.5">{label}</label>
             {
-                items?.map((item, index) => <Checkbox key={index} className={itemClassName} onChange={(e) => onCheckChange(index, e)} label={item.label} />)
+                items?.map((item, index) => {
+                    return <Checkbox key={index} className={itemClassName} checked={(status & (1 << index))} onChange={(e) => {
+                        let value = status;
+                        if (e.target.checked) {
+                            value = status | (1 << index)
+                        } else {
+                            value = status & (~(1 << index));
+                        }
+                        setStatus(value);
+                        onChange({ name, value });
+                    }} label={item.label} />
+                })
             }
         </div>
     )
 }
+
+export const CheckGroupString = ({ items, value }) => {
+    let str = "";
+    items.map((item, index) => {
+        if (value & (1 << index)) {
+            str += item.label;
+            if (index < items.length - 1)
+                str += "・";
+        }
+    })
+    return str;
+};
 
 export default CheckGroup;
