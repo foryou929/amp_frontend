bab89111f11cead127a44ef3bd9113c7e628eaf9
Alex Rio <ar02220604@gmail.com>
2024-02-29 17:28:11 +0900

    features: add-space-types&transportation-constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -19,31 +19,53 @@ export const SECTION = {
     REVIEW: 11
 }
 
-export const PROGRESS = {
+export const STEPS = {
     client: [
-        "提案",
-        "選定",
-        "承諾",
-        "仮払い",
-        "広告物発送",
-        "広告物受け取り",
-        "開始報告",
-        "経過報告",
-        "終了報告",
-        "支払い",
-        "レビュー",
+        { label: "提案" },
+        { label: "選定", button: "選定する", child: 0 },
+        { label: "承諾", button: "承諾する", child: 0 },
+        { label: "仮払い", button: "仮払いする", child: 1 },
+        { label: "広告物発送", button: "広告物発送する" },
+        { label: "広告物受け取り", },
+        { label: "開始報告" },
+        { label: "経過報告" },
+        { label: "終了報告" },
+        { label: "支払い" },
+        { label: "レビュー", button: "レビューする", child: 0 },
     ],
     user: [
-        "提案",
-        "選定",
-        "承諾",
-        "仮払い",
-        "広告物発送",
-        "広告物受け取り",
-        "開始報告",
-        "経過報告",
-        "終了報告",
-        "報酬受取",
-        "レビュー",
+        { label: "提案", button: "応募する", child: 0 },
+        { label: "選定" },
+        { label: "承諾" },
+        { label: "仮払い" },
+        { label: "広告物発送" },
+        { label: "広告物受け取り", button: "広告物受け取りする", child: 0 },
+        { label: "開始報告", button: "報告する", child: 0 },
+        { label: "経過報告", button: "報告する", child: 0 },
+        { label: "終了報告", button: "報告する", child: 0 },
+        { label: "報酬受取", button: "報酬受取" },
+        { label: "レビュー", button: "レビューする", child: 0 },
     ]
 }
+
+export const SPACE_TYPES = [
+    { label: "車の窓" },
+    { label: "車の車体" },
+    { label: "自転車" },
+    { label: "バイク" },
+    { label: "徒歩バックパック等" },
+    { label: "一軒家" },
+    { label: "集合住宅" },
+    { label: "ノベルティーグッズの使用" },
+    { label: "工事現場" },
+    { label: "看板" },
+    { label: "WEB SNS" },
+    { label: "他" },
+]
+
+export const TRANSPORTATIONS = [
+    { label: "徒歩" },
+    { label: "車" },
+    { label: "バイク" },
+    { label: "その他" },
+]
