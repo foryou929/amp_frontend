ca2779fd93876ec75e544afaeb8eba25976c61bd
Alex Rio <ar02220604@gmail.com>
2024-03-03 21:00:21 +0900

    features: add-url-change

--- src/components/Tab.jsx
+++ src/components/Tab.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 
-function Tab({tabs, ...rest}) {
-  const [activeTab, setActiveTab] = useState(0);
+function Tab({tabs, defaultActiveTab, onChangeTab, ...rest}) {
+  const [activeTab, setActiveTab] = useState(defaultActiveTab || 0);
 
   const handleTabClick = (index) => {
     setActiveTab(index);
+    if (onChangeTab) onChangeTab(index);
   };
 
   return (
@@ -13,7 +14,7 @@ function Tab({tabs, ...rest}) {
         {tabs.map((tab, index) => (
           <div
             key={index}
-            className={`flex-grow text-center border-b-2 py-2 cursor-pointer ${index === activeTab ? 'border-[#00146E] text-[#212529]' : 'border-[#CED4DA] text-[#92989D]'}`}
+            className={`flex-grow text-center border-b-2 py-2 cursor-pointer ${index == activeTab ? 'border-[#00146E] text-[#212529]' : 'border-[#CED4DA] text-[#92989D]'}`}
             onClick={() => handleTabClick(index)}
           >
             {tab.title}
