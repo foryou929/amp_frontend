43ade65ef02a828e260011a5d6ed5f248106c1bf
Alex Rio <ar02220604@gmail.com>
2024-03-03 23:13:45 +0900

    features: add-avatar-uploader-component

---
+++ src/components/AvatarUploader.jsx
@@ -0,0 +1,33 @@
+import { useRef, useState, forwardRef, useImperativeHandle } from "react";
+import Avatar from "./Avatar";
+import query from "../utils/query";
+
+const AvatarUploader = forwardRef(({ defaultSrc, className }, ref) => {
+    const fileRef = useRef();
+    const [src, setSrc] = useState(defaultSrc);
+    const [file, setFile] = useState(null);
+
+    const upload = (url, success, error) => {
+        const formData = new FormData();
+        formData.append('avatar', file);
+        query.auth.patch(url, formData, success, error);
+    }
+
+    useImperativeHandle(ref, () => ({
+        upload
+    }));
+
+    return (
+        <div className={className} onClick={() => fileRef.current.click()}>
+            <input type="file" className="hidden" ref={fileRef} accept="image/*" onChange={(e) => {
+                if (e.target.files.length == 1) {
+                    setSrc(URL.createObjectURL(e.target.files[0]));
+                    setFile(e.target.files[0]);
+                } else setSrc(defaultSrc);
+            }} />
+            <Avatar src={src} circle />
+        </div>
+    )
+})
+
+export default AvatarUploader;
