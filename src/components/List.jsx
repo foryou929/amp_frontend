ea1cf76ba1446a6aad867c3c7bf12e8610199d53
Alex Rio <ar02220604@gmail.com>
2024-03-03 20:52:23 +0900

    featurs: fix-list-component-bug

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -2,7 +2,7 @@ const List = ({ items, className, ...props }) => {
     return (
         <ul className={`border-t border-gray-200 ${className}`} {...props}>
             {
-                items.length == 0 ?
+                items?.length == 0 ?
                     <p className="py-4 text-center border-b border-gray-200">データはありません。</p>
                     : items?.map((item) => (
                         <li key={item.key} className="border-b border-gray-200 p-4">
