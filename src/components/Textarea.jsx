8792a4d5d7face6a86b80a6e31ceccce8ab44a52
Alex Rio <ar02220604@gmail.com>
2024-03-04 14:46:04 +0900

    features: add-textarea-reference

--- src/components/Textarea.jsx
+++ src/components/Textarea.jsx
@@ -1,7 +1,9 @@
-const Textarea = ({ className, ...rest }) => {
+import { forwardRef } from "react";
+
+const Textarea = forwardRef(({ className, ...rest }, ref) => {
     return (
-        <textarea className={`w-full border border-[#DEE2E6] focus:shadow outline-none p-2 ${className}`} {...rest}></textarea>
+        <textarea className={`w-full border border-[#DEE2E6] focus:shadow outline-none p-2 ${className}`} ref={ref} {...rest}></textarea>
     )
-}
+});
 
 export default Textarea;
