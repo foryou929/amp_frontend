eaa25dac9775f1e283221e7baf1f7b06b3f04702
Alex Rio <ar02220604@gmail.com>
2024-03-05 11:14:41 +0900

    features: add-space-manage-page

--- src/pages/Space/Manage.jsx
+++ src/pages/Space/Manage.jsx
@@ -1,6 +1,26 @@
-const Manage = () => {
+import { useEffect, useState } from "react";
+import List from "../../components/List";
+import SpaceItem from "../../components/SpaceItem";
+import query from "../../utils/query";
+
+const Manage = ({ mode }) => {
+    const [spaces, setSpaces] = useState([]);
+
+    useEffect(() => {
+        query.auth.get(`api/${mode}/space`, (spaces) => {
+            setSpaces(spaces);
+        });
+    }, []);
+
     return (
         <>
+            <h1 className="text-2xl font-bold">スペースを管理</h1>
+            <List className="mt-4" items={spaces.map(space => {
+                return {
+                    key: space.id,
+                    content: <SpaceItem mode={mode} space={space} />
+                }
+            })} />
         </>
     )
 }
