5c2a514c68e4998cb24f3f6fa9ba9f3497deeac5
Alex Rio <ar02220604@gmail.com>
2024-03-05 11:12:22 +0900

    features: change-link-component-style

--- src/components/Link.jsx
+++ src/components/Link.jsx
@@ -1,7 +1,7 @@
 import { NavLink } from "react-router-dom"
 
-const Link = ({ ...rest }) => {
-    return <NavLink className="text-blue-400" {...rest} />
+const Link = ({ className, ...rest }) => {
+    return <NavLink className={`text-[#00146E] ${className}`} {...rest} />
 }
 
 export default Link;
