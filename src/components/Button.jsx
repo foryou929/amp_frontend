b5909b8423835cff49304961c9ec0f55e21e7dfd
Alex Rio <ar02220604@gmail.com>
2024-03-05 11:07:43 +0900

    features: add-button-revert-style

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,6 @@
-const Button = ({ className, children, ...rest }) => {
+const Button = ({ className, children, revert, ...rest }) => {
     return (
-        <button className={`p-3 bg-[#00146E] text-white text-md font-bold rounded-lg ${className}`} {...rest}>{children}</button>
+        <button className={`p-3 ${revert ? "bg-white text-[#00146E] border border-[#00146E]" : "bg-[#00146E] text-white"} text-md font-bold rounded-lg ${className}`} {...rest}>{children}</button>
     )
 }
 
