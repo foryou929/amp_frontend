f4b30476c82c5483f14448bb3b8362c56abac302
Alex Rio <ar02220604@gmail.com>
2024-03-07 11:08:05 +0900

    features: fix-section-item-component-bug

--- src/components/SectionItem.jsx
+++ src/components/SectionItem.jsx
@@ -1,29 +1,37 @@
 import { NavLink } from "react-router-dom";
 
-// import Avatar from "./Avatar";
+import Avatar from "./Avatar";
+import Image from "./Image";
 import moment from "moment";
 import { STEPS } from "../utils/constants";
 
-const SectionItem = ({ mode, section }) => {
+const SectionItem = ({ mode, section, project }) => {
     return (
         <div className="flex gap-2">
             <div className="flex-grow flex gap-4 items-start">
-                <div className="flex-none w-12">
-                </div>
+                {
+                    mode == "client" ?
+                        <Avatar src={section.user.avatar} /> :
+                        project.project_images[0].length > 0 ?
+                            <Image className="w-12 flex-none" src={project.project_images} fallbackSrc={process.env.REACT_APP_BASE_URL + project.project_images[0].source} /> :
+                            <Image className="w-12 flex-none" src={"no-image"} />
+                }
                 <div className="flex-grow flex flex-col justify-center">
-                    <div className="w-full flex gap-2 items-center">
-                        <div className={`p-2 font-bold rounded ${section.step == 5 || section.step == 6 ? "bg-[#F08E1B] text-white" : "bg-[#E9ECEF] text-[#212529]"}`}>
-                            {STEPS[mode][section.step].label}
-                        </div>
-                        <p>{moment(section.project.updated_at).format("YYYY年MM月DD日")}</p>
-                    </div>
-                    <h3 className="text-lg font-bold text-[#00146E]">{section.project.name}</h3>
-                    <p className="font-bold text-sm mt-1">{section.project.points}pt</p>
-                    <p className="mt-1">{section.project.space_type}</p>
-                    {/*<div className="mt-1 p-2 bg-[#F8F9FA] text-sm flex justify-between">
-                        {section?.suggest_count && <p>提案数: {section.suggest_count}</p>} {section.project && <><p>募集数:{section.project.recruitment_number}</p> <p>期間:あと{moment(section.project.created_at).add(section.project.recruitment_period, 'days').diff(moment(section.project.current), 'days')}日</p></>}
-                        {section.project?.detail}
-                    </div> */}
+                    {
+                        project ?
+                            <>
+                                <div className="w-full flex gap-2 items-center">
+                                    <div className={`p-2 font-bold rounded ${section.step == 5 || section.step == 6 ? "bg-[#F08E1B] text-white" : "bg-[#E9ECEF] text-[#212529]"}`}>
+                                        {STEPS[mode][section.step].label}
+                                    </div>
+                                    <p>{moment(project.updated_at).format("YYYY年MM月DD日")}</p>
+                                </div>
+                                <h3 className="text-lg font-bold text-[#00146E]">{project.name}</h3>
+                                <p className="font-bold text-sm mt-1">{project.points}pt</p>
+                                <p className="mt-1">{project.space_type}</p>
+                            </> :
+                            <></>
+                    }
                     <div className="flex gap-2 items-start">
                         <div className="w-12 flex-none">
                         </div>
@@ -31,9 +39,6 @@ const SectionItem = ({ mode, section }) => {
                             <NavLink to={`/user/profile/info?id=${section.user.id}`} className="whitespace-nowrap overflow-hidden text-ellipsis font-bold">
                                 {section.user.username}
                             </NavLink>
-                            {/* <p className="whitespace-nowrap overflow-hidden text-ellipsis"> */}
-                            {/* {section.messages[0].content} */}
-                            {/* </p> */}
                             <p className="text-right text-sm text-gray-400">
                                 {moment(section.created_at).format("YYYY年MM月DD日 HH:mm")}
                             </p>
