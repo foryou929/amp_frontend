9bb62bcc5893d21a93430c80fb3db9f81f9c5e7c
Alex Rio <ar02220604@gmail.com>
2024-03-07 13:07:37 +0900

    features: update-login-page

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { NavLink, useNavigate } from "react-router-dom";
 
 import md5 from "md5";
 
@@ -38,23 +38,28 @@ const Login = () => {
     }
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
+        <div className="flex h-screen justify-center items-center">
+            <form className="max-w-[480px] px-8 py-16 border border-gray-200 shadow-xl" onSubmit={handleSubmit}>
                 <section className="py-2">
-                    <label>Username:</label>
+                    <label>ユーザーID:</label>
                     <div>
                         <Input className="w-full" onChange={(e) => setUserName(e.target.value)} />
                     </div>
                 </section>
                 <section className="py-2">
-                    <label>Password:</label>
+                    <label>パスワード:</label>
                     <Password className="w-full" onChange={(e) => setPassword(e.target.value)} />
                 </section>
                 <section className="py-2 flex gap-2">
-                    <Button className="flex-grow">Login</Button>
+                    <Button className="flex-grow">ログイン</Button>
+                </section>
+                <section className="text-right py-2">
+                    <NavLink className="text-[#56A7FF] font-bold" to={"/register"}>
+                        ユーザー登録ページで
+                    </NavLink>
                 </section>
             </form>
-        </>
+        </div>
     )
 }
 
