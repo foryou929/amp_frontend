8a320270b6f80969d36b4512842f7c23cd6f676c
Alex Rio <ar02220604@gmail.com>
2024-03-09 15:39:48 +0900

    features: change-user-profile-registration-page-axios-to-sync

--- src/pages/User/Profile/Registration.jsx
+++ src/pages/User/Profile/Registration.jsx
@@ -34,82 +34,79 @@ const Registration = ({ mode }) => {
         setProfile(newProfile);
     }
 
-    const handleSubmit = async(e) => {
+    const onSave = async (e) => {
         e.preventDefault();
         await avatarUploaderRef.current.upload(`/${mode}/${user.id}/avatar`);
-        query.auth.patch(`/${mode}/${user.id}`, profile, (user) => {
-            dispatch(login(user));
-            NotificationManager.success('Success');
-        });
+        const updatedUser = await query.auth.patch(`/${mode}/${user.id}`, profile);
+        dispatch(login(updatedUser));
+        NotificationManager.success('Success');
     }
 
     return (
         <>
             <h1 className="text-2xl font-bold">ユーザープロフィール</h1>
-            <form onSubmit={handleSubmit}>
-                <AvatarUploader className="py-4" defaultSrc={user.avatar} ref={avatarUploaderRef} />
-                <section className="py-2">
-                    <label className="py-0.5">ユーザー名</label>
-                    <Input className="w-full" name="username" value={profile.username} onChange={(e) => onChange(e.target)} />
-                </section>
-                <section className="py-2">
-                    <label className="py-0.5">種別</label>
-                    <Select name="type" value={profile.type} onChange={(e) => onChange(e.target)} />
-                </section>
-                <section className="py-2">
-                    <label className="py-0.5">エリア</label>
-                    <Select name="area" value={profile.area} onChange={(e) => onChange(e.target)} />
-                </section>
-                <section className="py-2">
-                    <CheckGroup label={"新有するスペース"} itemClassName={"py-1"}
-                        name="available_space" value={profile.available_space}
-                        onChange={(target) => onChange(target)}
-                        items={SPACE_TYPES} />
-                </section>
-                <section className="py-2">
-                    <label className="py-0.5">生年月日</label>
-                    <DatePicker name="birthday" value={profile.birthday} onChange={(target) => onChange(target)} />
-                </section>
-                <section className="py-2">
-                    <label className="py-0.5">性別</label>
-                    <Select name="gendor" value={profile.gendor} options={[{ value: 0, label: '男性' }, { value: 1, label: '女性' }]} onChange={(e) => onChange(e.target)} />
-                </section>
-                <section className="py-2">
-                    <label className="py-0.5">職業</label>
-                    <Select name="occupation" value={profile.occupation} onChange={(e) => onChange(e.target)} />
-                </section>
-                <section className="py-2">
-                    <label className="py-0.5">居住形態等</label>
-                    <Select name="residence_type" value={profile.residence_type} onChange={(e) => onChange(e.target)} />
-                </section>
-                <section className="py-2">
-                    <CheckGroup label={"主な移動手段"} itemClassName={"py-1"}
-                        name="primary_transportation" value={profile.primary_transportation}
-                        onChange={(target) => onChange(target)}
-                        items={TRANSPORTATIONS} />
-                </section>
-                <section className="py-2">
-                    <label className="py-0.5">活動範囲と頻度</label>
-                    <Textarea name="activity_scope" value={profile.activity_scope} onChange={(e) => onChange(e.target)} />
-                </section>
-                <section className="py-2">
-                    <label className="py-0.5">フォロワー数の目安</label>
-                    <div>
-                        <Input name="follower_count" value={profile.follower_count} onChange={(e) => onChange(e.target)} />
-                    </div>
-                </section>
-                <section className="py-2">
-                    <label className="py-0.5">使用頻度</label>
-                    <Select name="usage_frequency" value={profile.usage_frequency} onChange={(e) => onChange(e.target)} />
-                </section>
-                <section className="py-2">
-                    <label className="py-0.5">自己紹介文</label>
-                    <Textarea className="min-h-40" name="self_introduction" value={profile.self_introduction} onChange={(e) => onChange(e.target)} />
-                </section>
-                <section className="py-2">
-                    <Button className="w-full">保存</Button>
-                </section>
-            </form>
+            <AvatarUploader className="py-4" defaultSrc={user.avatar} ref={avatarUploaderRef} />
+            <section className="py-2">
+                <label className="py-0.5">ユーザー名</label>
+                <Input className="w-full" name="username" value={profile.username} onChange={(e) => onChange(e.target)} />
+            </section>
+            <section className="py-2">
+                <label className="py-0.5">種別</label>
+                <Select name="type" value={profile.type} onChange={(e) => onChange(e.target)} />
+            </section>
+            <section className="py-2">
+                <label className="py-0.5">エリア</label>
+                <Select name="area" value={profile.area} onChange={(e) => onChange(e.target)} />
+            </section>
+            <section className="py-2">
+                <CheckGroup label={"新有するスペース"} itemClassName={"py-1"}
+                    name="available_space" value={profile.available_space}
+                    onChange={(target) => onChange(target)}
+                    items={SPACE_TYPES} />
+            </section>
+            <section className="py-2">
+                <label className="py-0.5">生年月日</label>
+                <DatePicker name="birthday" value={profile.birthday} onChange={(target) => onChange(target)} />
+            </section>
+            <section className="py-2">
+                <label className="py-0.5">性別</label>
+                <Select name="gendor" value={profile.gendor} options={[{ value: 0, label: '男性' }, { value: 1, label: '女性' }]} onChange={(e) => onChange(e.target)} />
+            </section>
+            <section className="py-2">
+                <label className="py-0.5">職業</label>
+                <Select name="occupation" value={profile.occupation} onChange={(e) => onChange(e.target)} />
+            </section>
+            <section className="py-2">
+                <label className="py-0.5">居住形態等</label>
+                <Select name="residence_type" value={profile.residence_type} onChange={(e) => onChange(e.target)} />
+            </section>
+            <section className="py-2">
+                <CheckGroup label={"主な移動手段"} itemClassName={"py-1"}
+                    name="primary_transportation" value={profile.primary_transportation}
+                    onChange={(target) => onChange(target)}
+                    items={TRANSPORTATIONS} />
+            </section>
+            <section className="py-2">
+                <label className="py-0.5">活動範囲と頻度</label>
+                <Textarea name="activity_scope" value={profile.activity_scope} onChange={(e) => onChange(e.target)} />
+            </section>
+            <section className="py-2">
+                <label className="py-0.5">フォロワー数の目安</label>
+                <div>
+                    <Input name="follower_count" value={profile.follower_count} onChange={(e) => onChange(e.target)} />
+                </div>
+            </section>
+            <section className="py-2">
+                <label className="py-0.5">使用頻度</label>
+                <Select name="usage_frequency" value={profile.usage_frequency} onChange={(e) => onChange(e.target)} />
+            </section>
+            <section className="py-2">
+                <label className="py-0.5">自己紹介文</label>
+                <Textarea className="min-h-40" name="self_introduction" value={profile.self_introduction} onChange={(e) => onChange(e.target)} />
+            </section>
+            <section className="py-2">
+                <Button className="w-full" onClick={onSave}>保存</Button>
+            </section>
         </>
     )
 }
