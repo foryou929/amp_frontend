177e0a9aef7086110089b7ff2e248b498bf16817
Alex Rio <ar02220604@gmail.com>
2024-03-12 15:12:37 +0900

    features: add-space-status&space-steps-constants

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,6 +4,12 @@ export const PROJECT_STATUS = {
     FINISH: 2,
 }
 
+export const SPACE_STATUS = [
+    { value: 0, label: "募集中" },
+    { value: 1, label: "募集休止中" },
+    { value: 2, label: "募集開始予定" },
+]
+
 export const SECTION = {
     NORMAL: 0,
     APPLY: 1,
@@ -19,7 +25,7 @@ export const SECTION = {
     REVIEW: 11
 }
 
-export const STEPS = {
+export const PROJECT_STEPS = {
     client: [
         { label: "提案" },
         { label: "選定", button: "選定する", child: 1 },
@@ -34,12 +40,39 @@ export const STEPS = {
         { label: "レビュー", button: "レビューする", child: 3 },
     ],
     user: [
-        { label: "提案", button: "応募する", child: 1 },
+        { label: "提案" },
         { label: "選定" },
         { label: "承諾" },
         { label: "仮払い" },
         { label: "広告物発送" },
-        { label: "広告物受け取り", button: "広告物受け取りする", child: 1 },
+        { label: "広告物受け取り", button: "広告物受け取りする" },
+        { label: "開始報告", button: "報告する", child: 1 },
+        { label: "経過報告", button: "報告する", child: 1 },
+        { label: "終了報告", button: "報告する", child: 1 },
+        { label: "報酬受取" },
+        { label: "レビュー", button: "レビューする", child: 3 },
+    ]
+}
+
+export const SPACE_STEPS = {
+    client: [
+        { label: "提案" },
+        { label: "承諾" },
+        { label: "仮払い", button: "仮払いする", child: 2 },
+        { label: "広告物発送", button: "広告物発送する" },
+        { label: "広告物受け取り", },
+        { label: "開始報告" },
+        { label: "経過報告" },
+        { label: "終了報告" },
+        { label: "支払い", button: "報酬受取" },
+        { label: "レビュー", button: "レビューする", child: 3 },
+    ],
+    user: [
+        { label: "提案" },
+        { label: "承諾", button: "承諾する", child: 1 },
+        { label: "仮払い" },
+        { label: "広告物発送" },
+        { label: "広告物受け取り", button: "広告物受け取りする" },
         { label: "開始報告", button: "報告する", child: 1 },
         { label: "経過報告", button: "報告する", child: 1 },
         { label: "終了報告", button: "報告する", child: 1 },
