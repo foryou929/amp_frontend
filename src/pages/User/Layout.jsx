df95c1fae7a0281e3c80a16f5243276f4a54629a
Alex Rio <ar02220604@gmail.com>
2024-03-12 16:14:28 +0900

    features: add-user-layout-section-message-page-url

--- src/pages/User/Layout.jsx
+++ src/pages/User/Layout.jsx
@@ -6,16 +6,18 @@ import { user_menus } from "../menu";
 import Profile from '../User/Profile';
 import ProfileRegistration from './Profile/Registration';
 
+import ProjectApply from '../Project/Apply';
 import ProjectDetail from '../Project/Detail';
 import ProjectManage from '../Project/Manage';
-import ProjectView from '../Project/View';
-import ProjectApply from '../Project/Apply';
 import ProjectProgress from '../Project/Progress';
+import ProjectView from '../Project/View';
 
-import SpaceManage from '../Space/Manage';
-import SpaceRegistration from '../Space/Registration';
 import SpaceDetail from '../Space/Detail';
+import SpaceManage from '../Space/Manage';
 import SpaceProgress from '../Space/Progress';
+import SpaceRegistration from '../Space/Registration';
+
+import SectionMessage from '../Section/Message';
 
 const UserLayout = () => {
     return (
@@ -41,6 +43,8 @@ const UserLayout = () => {
                     <Route path="/space/registration" element={<SpaceRegistration mode={"user"} />} />
                     <Route path="/space/detail" element={<SpaceDetail mode={"user"} />} />
                     <Route path="/space/progress" element={<SpaceProgress mode={"user"} />} />
+
+                    <Route path="/section/message" element={<SectionMessage mode={"client"} />} />
                 </Routes>
             </div>
         </>
