13aa37270a9d8c19a8a81ce8e6c42111dc4302e0
Alex Rio <ar02220604@gmail.com>
2024-03-12 15:17:43 +0900

    features: update-space-detail-page

--- src/pages/Space/Detail.jsx
+++ src/pages/Space/Detail.jsx
@@ -8,20 +8,21 @@ import Button from "../../components/Button";
 import Image from "../../components/Image";
 import Ranking from "../../components/Ranking";
 
+import { SPACE_TYPES, AREAS, SPACE_STATUS } from "../../utils/constants";
 import query from "../../utils/query";
 
 const Detail = ({ mode }) => {
     const navigate = useNavigate();
 
-    const [space, setSpace] = useState({ space_images: [] });
+    const [space, setSpace] = useState({});
+    const [section, setSection] = useState({});
 
     const [queryParameters] = useSearchParams();
     const id = queryParameters.get("id");
 
     useEffect(() => {
-        query.auth.get(`/${mode}/space/${id}`, (space) => {
-            setSpace(space);
-        });
+        query.auth.get(`/${mode}/space/${id}`, (space) => setSpace(space));
+        query.auth.get(`/${mode}/section/space/${id}`, section => setSection(section));
     }, []);
 
     return (
@@ -29,18 +30,15 @@ const Detail = ({ mode }) => {
             <Backward>戻る</Backward>
             <div className="mt-4">
                 {
-                    space.space_images.length >= 1 ?
-                        <Image className="flex-none w-full" src={space.space_images[0].source} /> :
+                    space.space_images?.length >= 1 ?
+                        <Image className="flex-none w-full" src={space.space_images[0]?.source} /> :
                         <Image className="flex-none w-full" src={"/img/no-image.svg"} />
                 }
                 {
-                    space.space_images.length >= 2 ?
+                    space.space_images?.length >= 2 ?
                         <div className="overflow-x-scroll h-32 mt-4 flex gap-2">
                             {
-                                space.space_images.map((image, index) => {
-                                    if (index == 0) return <></>
-                                    return <Image className="flex-none h-full" src={image.source} />
-                                })
+                                space.space_images.filter((_, index) => index != 0).map((image) => <Image key={image.id} className="flex-none h-full" src={image.source} />)
                             }
                         </div> :
                         <></>
@@ -59,11 +57,11 @@ const Detail = ({ mode }) => {
             <ul className="mt-2">
                 <li className="border-b border-[#DEE2E6] px-2 py-3 flex">
                     <p className="w-1/2">スペース種別</p>
-                    <p className="w-1/2">{space.suggest_count}</p>
+                    <p className="w-1/2">{SPACE_TYPES.filter(space_type => space_type.value == space.type)[0]?.label}</p>
                 </li>
                 <li className="border-b border-[#DEE2E6] px-2 py-3 flex">
                     <p className="w-1/2">期間</p>
-                    <p className="w-1/2">{space.period}</p>
+                    <p className="w-1/2">{space.period}週間</p>
                 </li>
                 <li className="border-b border-[#DEE2E6] px-2 py-3 flex">
                     <p className="w-1/2">価格</p>
@@ -71,23 +69,23 @@ const Detail = ({ mode }) => {
                 </li>
                 <li className="border-b border-[#DEE2E6] px-2 py-3 flex">
                     <p className="w-1/2">エリア</p>
-                    <p className="w-1/2">{space.suggest_count}</p>
+                    <p className="w-1/2">{AREAS.filter(area => area.value == space.area)[0]?.label}</p>
                 </li>
                 <li className="border-b border-[#DEE2E6] px-2 py-3 flex">
                     <p className="w-1/2">ステータス</p>
-                    <p className="w-1/2">{space.suggest_count}</p>
+                    <p className="w-1/2">{SPACE_STATUS.filter(space_status => space_status.value == space.status)[0]?.label}</p>
                 </li>
-                <li className="border-b border-[#DEE2E6] px-2 py-3 flex">
+                {/* <li className="border-b border-[#DEE2E6] px-2 py-3 flex">
                     <p className="w-1/2">取引トラブル等回数</p>
                     <p className="w-1/2">{space.suggest_count}</p>
-                </li>
+                </li> */}
                 <li className="border-b border-[#DEE2E6] px-2 py-3 flex">
                     <p className="w-1/2">レビュー</p>
-                    <p className="w-1/2">
+                    <div className="w-1/2">
                         <Ranking rank={3}>
                             <span className="text-[#00146E]">30</span>
                         </Ranking>
-                    </p>
+                    </div>
                 </li>
             </ul>
             {
@@ -110,7 +108,21 @@ const Detail = ({ mode }) => {
                             </div>
                         </div >
                         <Button className="w-full mt-4" revert>お気に入りに追加</Button>
-                        <Button className="w-full mt-16" onClick={() => navigate(`/client/space/apply?id=${id}`)}>依頼する</Button>
+                        {
+                            section.id ?
+                                <Button className="w-full mt-4" onClick={() => navigate(`/client/space/progress?id=${section.id}`)}>スペース進捗ページへ</Button> :
+                                <Button className="w-full mt-4" onClick={() => navigate(`/client/space/apply?id=${id}`)}>依頼する</Button>
+                        }
+                    </>
+                )
+            }
+            {
+                mode == "user" && (
+                    <>
+                        <Button className="w-full mt-4" onClick={() => navigate(`/user/space/registration?id=${id}`)} revert>スペース編集</Button>
+                        {
+                            section.id && <Button className="w-full mt-4" onClick={() => navigate(`/user/space/progress?id=${section.id}`)}>スペース進捗ページへ</Button>
+                        }
                     </>
                 )
             }
