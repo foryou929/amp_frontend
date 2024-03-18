83c7eaff06013b9432c3543f7f2e3ba74ce2777f
Alex Rio <ar02220604@gmail.com>
2024-03-18 19:29:31 +0900

    update 404 page

---
+++ src/pages/Page404.jsx
@@ -0,0 +1,9 @@
+const Page404 = () => {
+    return (
+        <div className="w-screen h-screen flex justify-center items-center">
+            No Page
+        </div>
+    )
+}
+
+export default Page404;
