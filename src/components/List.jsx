4bf9378bf8d5782dedf914efde22f01e4d2ec9ca
foryou929 <zxcvwert3457@gmail.com>
2024-03-26 22:59:47 +0900

    update list component

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,8 @@
-const List = ({ items, className, ...props }) => {
+const List = ({ items, className, limit, ...props }) => {
     return (
         <ul className={`border-t border-gray-200 ${className}`} {...props}>
             {
-                items?.length == 0 ?
+                items?.filter((_, index) => limit ? index < limit : true).length == 0 ?
                     <p className="py-4 text-center border-b border-gray-200">データはありません。</p>
                     : items?.map((item) => (
                         <li key={item.key} className="border-b border-gray-200 p-4">
